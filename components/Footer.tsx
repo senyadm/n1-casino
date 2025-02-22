94e1bbb6042ea93af63fab2e71c560c64a8ac769
Arseniy Mezentsev <arseniymezentsev@MacBook-Air-Arseniy.local>
2025-02-22 15:22:05 +0000

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-n1gray text-center py-6">
       <p className="text-sm text-white">
-        © 2025 N1 Casino. All rights reserved.
+        © 2025 N1 Casino. Με την επιφύλαξη παντός δικαιώματος.
       </p>
     </footer>
   );
